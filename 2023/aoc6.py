192ba387f0b4e1f62dcb21f391bdec072af204b4
itssoap <soaibkhan38@gmail.com>
2023-12-04 02:11:52 +0530

    AOC day 1-3a

--- 2023/aoc6.py
+++ 2023/aoc6.py
@@ -1,4 +1,4 @@
-with open(r"C:\Users\soaib\Documents\Projects\advent-of-code\2023\aoc5.txt", "r") as f:
+with open(r"aoc6.txt", "r") as f:
     lines = f.readlines()
     
 lines = [line.strip() for line in lines]
